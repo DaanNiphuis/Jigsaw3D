b4e04c674722f6c4c9cd5a0f9a2753354d92c3d5
Daan Niphuis <dniphuis@gmail.com>
2012-08-22 00:26:14 +0200

    Linux preparation debug.cpp

--- source/Jigsaw3D/Debug.cpp
+++ source/Jigsaw3D/Debug.cpp
@@ -9,10 +9,16 @@
 void printAssertion(const std::string& p_message, const char* p_file)
 {
 	MessageBoxA(0, 
-				std::string("Assertion failed! " + p_message + 
-							"\nFile: " + p_file).c_str(),
+				std::string("Assertion failed! " + p_message + "\nFile: " + p_file).c_str(),
 				"Assertion failed.",
 				MB_ICONERROR | MB_OK);
+}
+
+#elif defined __linux__
+
+void printAssertion(const std::string& p_message, const char* p_file)
+{
+	not implemented
 }
 
 #else
